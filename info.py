bd1ca5dd3ff244b30b0ba97b16ad00abdf26690a
Anjan Budige Altruist <61613373+anjan-budige@users.noreply.github.com>
2023-09-30 20:59:31 +0530

    Create info.py

---
+++ info.py
@@ -0,0 +1,22 @@
+# Bot information
+SESSION = 'Media_search'
+USER_SESSION = 'User_Bot'
+API_ID = 15914139
+API_HASH = "f5c54e1dd806604552e211b3841c1ad4"
+BOT_TOKEN = "5635079039:AAFulnoUbDmBtTyHcMYwTbvTAQw5EsABE3Q"
+USERBOT_STRING_SESSION = ''
+
+# Bot settings
+CACHE_TIME = 300
+USE_CAPTION_FILTER = False
+
+# Admins, Channels & Users
+ADMINS = 1121140181
+CHANNELS = [-1001313414450]
+AUTH_USERS = [1121140181]
+AUTH_CHANNEL = None
+
+# MongoDB information
+DATABASE_URI = "mongodb+srv://sushankm16:4i1WAfPYKWyqPIDD@cluster0.sngp9pz.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_NAME = "M4uVideo_bot"
+COLLECTION_NAME = 'Telegram_files'  # If you are using the same database, then use different collection name for each bot
